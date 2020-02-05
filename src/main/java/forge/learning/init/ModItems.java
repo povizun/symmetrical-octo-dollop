99fbded589a1599de3aeb9629d4c1d16dbd107ff
Povizun <povizun@bk.ru>
2020-02-05 16:40:51 +0300

    First Dust Complete

--- src/main/java/forge/learning/init/ModItems.java
+++ src/main/java/forge/learning/init/ModItems.java
@@ -14,16 +14,20 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @Mod.EventBusSubscriber(modid=Reference.MODID)
 public class ModItems {
 	static Item firstItem;
+	static Item firstDust;
 	public static void init() {
-		firstItem = new ItemBasic("first_item").setCreativeTab(CreativeTabs.MATERIALS).setMaxStackSize(32);
+		firstItem = new ItemBasic("first_item").setCreativeTab(CreativeTabs.MATERIALS);
+		firstDust = new ItemBasic("first_dust").setCreativeTab(CreativeTabs.MATERIALS);
 	}
 	@SubscribeEvent
 	public static void registerItems(RegistryEvent.Register<Item> event) {
 		event.getRegistry().registerAll(firstItem);
+		event.getRegistry().registerAll(firstDust);
 	}
 	@SubscribeEvent
 	public static void registerRenders(ModelRegistryEvent event) {
 		registerRender(firstItem);
+		registerRender(firstDust);
 	}
 	private static void registerRender(Item item) {
 		ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation( item.getRegistryName(), "inventory"));
